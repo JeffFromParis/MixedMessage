0b1348b5b61bcd4a4c35dcead4448d04ec1421af
Jeff Vergne <vergnejf@gmail.com>
2021-09-24 21:36:31 +0200

    add messages from la cite de la peur

---
+++ messages.js
@@ -0,0 +1,26 @@
+const citePeur = [
+    "On peut tromper mille fois mille personnes, non, on peut tromper une fois mille personnes, mais on ne peut pas tromper mille fois mille personnes. Non, on peut tromper une fois mille personne mais on peut pas tromper mille fois une personne. Non...",
+    "- Vous voulez un whisky ? - Non, juste un doigt. - Vous voulez pas un whisky d'abord ?",
+    "Quelle est la différence entre une moule et un pull-over ? ... Bah un pull-over ça moule et une moule sa pue l'ovaire !",
+    "Simon, t'as un bout de pomme de terre sur la joue.",
+    "- Vous êtes Odile Deray ? - Non, je suis le Pape et j'attends ma soeur ! - ... - C'est moi ! - Ah ! Je suis envoyé par l'agence de security. - Et vous êtes ? - A l'aréoport. - Ça je sais, mais votre nom c'est quoi ? - Ah ! Serge... Karamazov. Aucun lien je suis fils unique.",
+    "Prenez un chewing gum Emile.",
+    "- Parlez moi de vous plutôt. - Odile ! Moi c'est Odile ! Pluto c'est l'ami de Mickey ! - Ah non ! Pluto c'est le chien de Mickey ! L'ami de Mickey c'est Dingo !",
+    "- On peut se tutoyer ? Hein c'est plus sympa. - Ouais. - T'es lourd.",
+    "- Madame je vous pisse à la raie ! - Deray, Odile Deray...",
+    "- Mademoiselle Deray je dois vous informer que cet entretien sera filmé intégralement. - Ça va pas être possible commissaire... Il est pété le truc. C'est Bestell il a mal mis la cassette t'à l'heure et ça a tout niqué ! - Mademoiselle Deray je dois vous informer que cet entretien sera intégralement pas filmé.",
+    "Il dit qu'il a mal au genou... Il dit qu'il voit pas le rapport.",
+    "Karamazov... Aucun lien je suis fils unique.",
+    "Barrez-vous, cons de mimes !",
+    "Merci Émile, vous me retirez une fière chandelle du pied.",
+    "- Je m'occupais du festival moutarde et cinéma. - Ah ! c'est passionnant, ça. Et ça parlait de quoi? - De moutarde et de cinéma. - Oh ! Formidable ! - Ma première voiture, c'était une Peugeot ! - Oh, c'est formidable. - Et un jour, mes cousins sont allés coucher chez ma grand-mère ! - Oh, c'est fabuleux !",
+    "- Il s'est fait coincer par la tapette géante ! - Écoutez Kara, Monsieur n'est pas une tapette, Monsieur est commissaire de police !",
+    "Émile, vous m'avez sauvé ma vie ! Oh, je vous dois une fière bretelle !",
+    "Meurs, pourriture communiste !",
+    "Il ne nous aimait pas parce que nous gagnions de l'argent, et dans son optique prolétarienne le profit lui était insoutenable.",
+    "- Tu prends quel lit Simon ? - ... [Il vomit sur un lit] - Ok donc moi je prends l'autre...",
+    "- Dites, Simon, il va falloir me le vendre ce film... Trouvez des gimmicks, des phrases chocs. - Quel genres ? - Heu... Genre, genre... Genre des phrases chocs - Ah."
+];
+const oss = [];
+const cEstArrive = [];
+const messages = [citePeur,oss,cEstArrive];
